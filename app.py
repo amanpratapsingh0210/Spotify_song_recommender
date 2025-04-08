944930ea5db06f1b24c2883bba90f0c36914a0a9
AmanPSingh <amanp.singh0210@gmail.com>
2025-04-08 13:43:38 +0530

    deploy ready

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def recommend():
     recommendations = recommend_song(track_id)
     return jsonify(recommendations)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=False)
